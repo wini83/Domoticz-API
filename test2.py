7cbc987067974a3389e514abdbc19f03f14f364b
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-04-26 21:19:10 +0200

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import DomoticzAPI as dom
+
+
+def main():
+    server = dom.Server(address="192.168.0.13")
+    print(server)
+
+
+if __name__ == "__main__":
+    main()
