213939b5dd734589d7f009d9c53c527eb489cd34
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-04-29 20:07:58 +0200

    Create notification.py

---
+++ DomoticzAPI/notification.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from urllib.parse import quote
+
+
+################################################################################
+# Notification                                                                       #
+################################################################################
+class Notification:
+
+    _param_notification = "sendnotification"
+
+    _subsystems = {
+        "gcm",
+        "http",
+        "kodi",
+        "lms",
+        "nma",
+        "prowl",
+        "pushalot",
+        "pushbullet",
+        "pushover",
+        "pushsafer",
+    }
+
+    def __init__(self, server, subject=None, body=None, subsystem=None):
+        self._server = server
+        self._subject = subject
+        self._body = body
+        if subsystem in self._subsystems:
+            self._subsystem = subsystem
+        else:
+            self._subsystem = None
+        self._status = ""
+        self._title = ""
+
+    def __str__(self):
+        return "{}({})".format(self.__class__.__name__, self._subject)
+
+    # ..........................................................................
+    # Properties
+    # ..........................................................................
+    @property
+    def body(self):
+        return self._body
+
+    @body.setter
+    def body(self, value):
+        self._body = value
+
+    @property
+    def server(self):
+        return self._server
+
+    @property
+    def status(self):
+        return self._status
+
+    @property
+    def subject(self):
+        return self._subject
+
+    @subject.setter
+    def subject(self, value):
+        self._subject = value
+
+    @property
+    def subsystem(self):
+        return self._subsystem
+
+    @subsystem.setter
+    def subsystem(self, value):
+        if value in self._subsystems:
+            self._subsystem = value
+        else:
+            self._subsystem = None
+
+    @property
+    def title(self):
+        return self._title
+
+    # ..........................................................................
+    # Public methods
+    # ..........................................................................
+    def send(self):
+        if self._subject is not None and self._body is not None:
+            message = self._server._param.format(self._param_notification) + "&subject={}&body={}".format(quote(self._subject), quote(self._body))
+            if self._subsystem is not None:
+                message += "&subsystem={}".format(self._subsystem)
+            res = self._server._call_command(message)
+            self._status = res["status"]
+            if self._status == self._server._return_ok:
+                self._title = res["title"]
