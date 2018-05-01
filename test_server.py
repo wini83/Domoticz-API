f20ada9fbb3e1e01e6731cb1b473058acca8f751
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-05-01 21:20:51 +0200

    Added datetime test

--- test_server.py
+++ test_server.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import DomoticzAPI as dom
-
+from datetime import datetime
 
 def main():
     server = dom.Server(address="192.168.0.13")
     print(server)
-    print("ServerTime = " + server.ServerTime)
-    print("Sunrise = " + server.Sunrise)
-    print("Sunset = " + server.Sunset)
+    print("Server = " + server.server)
+    print("ServerDT = " + str(server.server_dt))
+    print("Sunrise = " + server.sunrise)
+    print("Sunset = " + server.sunset)
+    print("SunsetDT = " + str(server.sunset_dt))
+    print("Status = " + server.status)
+    print("Title = " + server.title)
     server.logmessage("Test")
-    print("Status = " + server.Status)
+    print("Status = " + server.status)
+    print("Title = " + server.title)
 
 if __name__ == "__main__":
     main()
