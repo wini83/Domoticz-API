69588769c9d966cb747af84c17aef758aeaf5e4f
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-05-03 21:36:28 +0200

    Update test_device.py

--- test_device.py
+++ test_device.py
@@ -5,16 +5,22 @@ import DomoticzAPI as dom
 
 def main():
     server = dom.Server(address="192.168.0.13")
+    # Existing device
     dev1 = dom.Device(server, "17")
     print(dev1)
+    print("Value: " + dev1.data)
     if dev1.exists():
         print("Has battery = " + str(dev1.hasBattery()))
         print("Hardware ID = " + str(dev1.hardwareid))
+        hw1 = dom.Hardware(server, str(dev1.hardwareid))
+        print(hw1)
     else:
         print("Device " + dev1.idx + " doesn't exists or is disabled")
-    # print(vars(dev1))
-    dir(dev1)
 
+    # Non existing device
+    dev2 = dom.Device(server, name="PiMonitor - Connections")
+    print(dev2)
+    print("Value: " + dev2.data)
 
 if __name__ == "__main__":
     main()
