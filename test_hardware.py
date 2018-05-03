7cd64dae612d34f94389162ddad688768e49662b
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-05-03 21:37:40 +0200

    Update test_hardware.py

--- test_hardware.py
+++ test_hardware.py
@@ -6,17 +6,19 @@ import DomoticzAPI as dom
 def main():
     server = dom.Server(address="192.168.0.13")
     # hw1 = dom.Hardware(server, "180")
-    hw1 = dom.Hardware(server, idx="180")
+    hw1 = dom.Hardware(server, idx="18")
     print(hw1)
+    print("{}: {} {}".format(hw1.name, hw1.status, hw1.title))
     if hw1.exists():
-        print("Name = " + str(hw1.Name))
+        print("Name = " + str(hw1.name))
     else:
         print("Hardware " + hw1.idx + " doesn't exists")
 
     hw2 = dom.Hardware(server, Type=15, Port=1, Name="Sensors1", Enabled="true")
     hw2.add()
-    print(hw2.status)
     print(hw2)
+    print("{}: {} {}".format(hw2.name, hw2.status, hw2.title))
+
 
 if __name__ == "__main__":
     main()
