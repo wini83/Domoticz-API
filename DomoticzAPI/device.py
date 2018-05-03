0ddbd99c313b5fbd4b5fe2f65d820e42cae42a7b
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-05-03 21:30:43 +0200

    Update device.py

--- DomoticzAPI/device.py
+++ DomoticzAPI/device.py
@@ -1,50 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from .server import Server
+from .hardware import Hardware
+
+
 ################################################################################
 # Device                                                                       #
 ################################################################################
 class Device:
+    _type_devices = "devices"
+    _type_create_device = "createdevice"
 
-    def __init__(self, server, idx):
-        self._server = server
-        self._idx = idx
-        self._status = ""
-        self._AddjMulti = None
-        self._AddjMulti2 = None
-        self._AddjValue = None
-        self._AddjValue2 = None
-        self._BatteryLevel = None
-        self._CustomImage = None
-        self._Data = None
-        self._Description = None
-        self._Favorite = None
-        self._HardwareID = None
-        self._HardwareName = None
-        self._HardwareType = None
-        self._HardwareTypeVal = None
-        self._HaveTimeout = None
-        self._ID = None
-        self._Image = None
-        self._LastUpdate = None
-        self._Name = None
-        self._Notifications = None
-        self._PlanID = None
-        self._PlanIDs = None
-        self._Protected = None
-        self._SensorType = None
-        self._SensorUnit = None
-        self._ShowNotifications = None
-        self._SignalLevel = None
-        self._SubType = None
-        self._Timers = None
-        self._Type = None
-        self._TypeImg = None
-        self._Unit = None
-        self._Used = None
-        self._XOffset = None
-        self._YOffset = None
-        self._getDevice()
+    # Existing device: def __init__(self, server, idx)
+    # New device:      def __init__(self, server, hardwareidx, name, type=None, subtype=None):
+    def __init__(self, server, *args, **kwargs):
+        if isinstance(server, Server) and server.exists():
+            self._server = server
+        else:
+            self._server = None
+        if len(args) == 1:
+            # For existing hardware
+            #   hw = dom.Hardware(server, "180")
+            self._idx = args[0]
+        elif len(args) >= 2:
+            self._idx = None
+            if isinstance(args[0], Hardware):
+                if args[0].exists():
+                    self._hardware = args[0]
+                else:
+                    self._hardware = None
+            self._Name = args[1]
+            if len(args) >= 3:
+                self._HardwareType = args[2]
+                if len(args) >= 4:
+                    self._SubType = args[3]
+                else:
+                    self._SubType = None
+            else:
+                self._HardwareType = None
+        else:
+            self._idx = kwargs.get("idx", None)
+            if self._idx is None:
+                self._Name = kwargs.get("name", None)
+                self._HardwareType = kwargs.get("type", None)
+                self._SubType = kwargs.get("subtype", None)
+        self._initDevice()
 
     def __str__(self):
         return "{0}({1}, \"{2}\", \"{3}\")".format(self.__class__.__name__, str(self._server), self._idx, self._Name)
@@ -53,7 +54,21 @@ class Device:
     # Public methods
     # ..........................................................................
     def exists(self):
-        return self._HardwareID is not None
+        return self._HardwareID is not None and self._idx is not None
+
+    def add(self):
+        if self._idx is None \
+                and self._HardwareID is not None \
+                and self._Name is not None \
+                and self._HardwareType is not None \
+                and self._SubType is not None:
+            # type=createdevice&idx=29&sensorname=Temp5&devicetype=50&devicesubtype=5
+            message = "type={}&idx={}&sensorname={}&devicetype={}&devicesubtype={}".format(self._type_create_device,
+                                                                                           self._HardwareID,
+                                                                                           self._Name,
+                                                                                           self._HardwareType,
+                                                                                           self._SubType)
+        pass
 
     def hasBattery(self):
         return not (self._BatteryLevel is None or self._BatteryLevel == 255)
@@ -173,6 +188,10 @@ class Device:
         return self._Timers
 
     @property
+    def title(self):
+        return self._title
+
+    @property
     def type(self):
         return self._Type
 
@@ -203,51 +222,56 @@ class Device:
     # ..........................................................................
     # Private methods
     # ..........................................................................
-    def _getDevice(self):
-        message = "type=devices&rid={}".format(self._idx)
+    def _initDevice(self):
+        if self._idx is not None:
+            message = "type=devices&rid={}".format(self._idx)
+        elif self._Name is not None:
+            message = "type=devices&filter=all&used=true&order=Name"
+        else:
+            message = ""
         res = self._server._call_api(message)
-        res = res["result"] if res.get("result") else ""
-        if len(res) > 0:
-            for myDict in res:
-                if myDict["idx"] == self._idx:
-                    # Used one line if ... then ... else for more compact code
-                    self._AddjMulti = myDict["AddjMulti"]
-                    self._AddjMulti2 = myDict["AddjMulti2"]
-                    self._AddjValue = myDict["AddjValue"]
-                    self._AddjValue2 = myDict["AddjValue2"]
-                    self._BatteryLevel = myDict["BatteryLevel"] if myDict.get("BatteryLevel") else 255
-                    self._CustomImage = myDict["CustomImage"]
-                    self._Data = myDict["Data"]
-                    self._Description = myDict["Description"]
-                    self._Favorite = myDict["Favorite"]
-                    self._HardwareID = myDict["HardwareID"] if myDict.get("HardwareID") else -1
-                    #self._HardwareID = myDict["HardwareID"]
-                    self._HardwareName = myDict["HardwareName"]
-                    self._HardwareType = myDict["HardwareType"]
-                    self._HardwareTypeVal = myDict["HardwareTypeVal"]
-                    self._HaveTimeout = myDict["HaveTimeout"]
-                    self._ID = myDict["ID"]
-                    self._Image = self._EmptyIfNotExist(myDict, "Image")
-                    self._LastUpdate = myDict["LastUpdate"]
-                    self._Name = myDict["Name"]
-                    self._Notifications = myDict["Notifications"]
-                    self._PlanID =myDict["PlanID"]
-                    self._PlanIDs = myDict["PlanIDs"]
-                    self._Protected = myDict["Protected"]
-                    self._SensorType = self._EmptyIfNotExist(myDict, "SensorType")
-                    self._SensorUnit = self._EmptyIfNotExist(myDict, "SensorUnit")
-                    self._ShowNotifications = myDict["ShowNotifications"]
-                    self._SignalLevel = myDict["SignalLevel"]
-                    self._SubType = myDict["SubType"]
-                    self._Timers = myDict["Timers"]
-                    self._Type = myDict["Type"]
-                    self._TypeImg = myDict["TypeImg"]
-                    self._Unit = myDict["Unit"]
-                    self._Used = myDict["Used"]
-                    self._XOffset = myDict["XOffset"]
-                    self._YOffset = myDict["YOffset"]
-
-
-    def _EmptyIfNotExist(self, d, attr):
-        res = d[attr] if d.get(attr) else ""
-        return res
+        result = res.get("result")
+        myDict = {}
+        if len(result) > 0:
+            for myDict in result:
+                if (self._idx is not None and myDict.get("idx") == self._idx) \
+                        or (self._Name is not None and myDict.get("Name") == self._Name):
+                    break
+        self._AddjMulti = myDict.get("AddjMulti")
+        self._AddjMulti2 = myDict.get("AddjMulti2")
+        self._AddjValue = myDict.get("AddjValue")
+        self._AddjValue2 = myDict.get("AddjValue2")
+        self._BatteryLevel = myDict.get("BatteryLevel", 255)
+        self._CustomImage = myDict.get("CustomImage")
+        self._Data = myDict.get("Data")
+        self._Description = myDict.get("Description")
+        self._Favorite = myDict.get("Favorite")
+        self._HardwareID = myDict.get("HardwareID")
+        self._HardwareName = myDict.get("HardwareName")
+        self._HardwareType = myDict.get("HardwareType")
+        self._HardwareTypeVal = myDict.get("HardwareTypeVal")
+        self._HaveTimeout = myDict.get("HaveTimeout")
+        self._ID = myDict.get("ID")
+        self._idx = myDict.get("idx")
+        self._Image = myDict.get("Image")
+        self._LastUpdate = myDict.get("LastUpdate")
+        self._Name = myDict.get("Name")
+        self._Notifications = myDict.get("Notifications")
+        self._PlanID = myDict.get("PlanID")
+        self._PlanIDs = myDict.get("PlanIDs")
+        self._Protected = myDict.get("Protected")
+        self._SensorType = myDict.get("SensorType")
+        self._SensorUnit = myDict.get("SensorUnit")
+        self._ShowNotifications = myDict.get("ShowNotifications")
+        self._SignalLevel = myDict.get("SignalLevel")
+        self._SubType = myDict.get("SubType")
+        self._Timers = myDict.get("Timers")
+        self._Type = myDict.get("Type")
+        self._TypeImg = myDict.get("TypeImg")
+        self._Unit = myDict.get("Unit")
+        self._Used = myDict.get("Used")
+        self._XOffset = myDict.get("XOffset")
+        self._YOffset = myDict.get("YOffset")
+        #
+        self._status = res.get("status")
+        self._title = res.get("title")
