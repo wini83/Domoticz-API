926d831d0bdc37804a6bc4c935d2a11cdd05ee79
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-05-16 22:15:36 +0200

    Added extra tests and print() lines to check

--- test_uservariable.py
+++ test_uservariable.py
@@ -1,43 +1,77 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# For this test, first create a uservariable in Domoticz with the name Test0
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 import DomoticzAPI as dom
 
 
 def main():
-    server = dom.Server(address="192.168.0.13")
+    #server = dom.Server(address="192.168.0.13")
+    #server = dom.Server(address="127.0.0.1")
+    server = dom.Server()
 
+    # Define an user variable only by name, which does exists in Domoticz
+    var0 = dom.UserVariable(server, "Test0")
+    print(var0)
+    print("Check {}".format(var0.name))
+    if var0.exists():
+        print("{} exists in Domoticz".format(var0.name))
+    else:
+        print("{} NOT exists in Domoticz".format(var0.name))
+
+    print("\n")
+    # Define an user variable only by name, which does not exists in Domoticz
     var1 = dom.UserVariable(server, "Test1")
-    var1.add()
-    if var1.exists():
-        print("var1 exists in Domoticz")
     print(var1)
+    print("Check {}".format(var1.name))
+    if var1.exists():
+        print("{} exists in Domoticz".format(var1.name))
+    else:
+        print("{} NOT exists in Domoticz".format(var1.name))
 
-    var2 = dom.UserVariable(server, "Test2", "float", "3.21")
+    print("\n")
+    # Create an new user variable
+    var2 = dom.UserVariable(server, "Test2", "float", "1.23")
+    print("Add {}".format(var2.name))
     var2.add()
-    var2.value = "11.87"
-    var2.update()
     print(var2)
     if var2.exists():
-        print("var2 exists in Domoticz")
+        print("{} exists in Domoticz".format(var2.name))
+    else:
+        print("{} NOT exists in Domoticz".format(var2.name))
+    var2.value = "11.87"
+    print("Update {}".format(var2.name))
+    var2.update()
+    print(var2)
+    print("Delete {}".format(var2.name))
+    var2.delete()
+    if var2.exists():
+        print("{} exists in Domoticz".format(var2.name))
+    else:
+        print("{} NOT exists in Domoticz".format(var2.name))
 
-    var3 = dom.UserVariable(server, "Test3", "float", "1.23")
-    var3.value = 4.56
+    print("\n")
+    var3 = dom.UserVariable(server, "Test3", "integer")
+    print(var3)
+    flt = 4.56
+    print("Update value for {}({}) with: {}".format(var3.name, var3.type, flt))
+    var3.value = flt
+    print("Add {}".format(var3.name))
     var3.add()
     print(var3)
     if var3.exists():
-        print("var3 exists in Domoticz")
-
-    var4 = dom.UserVariable(server, "Test4", "integer")
-    var4.value = 4.56
-    var4.add()
-    var4.update()
-    if var4.exists():
-        print("var4 exists in Domoticz")
-    print(var4)
-
-    var1.delete()
-    var2.delete()
+        print("{} exists in Domoticz".format(var3.name))
+    else:
+        print("{} NOT exists in Domoticz".format(var3.name))
+    print("Delete {}".format(var3.name))
     var3.delete()
+    if var3.exists():
+        print("{} exists in Domoticz".format(var3.name))
+    else:
+        print("{} NOT exists in Domoticz".format(var3.name))
 
 
 if __name__ == "__main__":
