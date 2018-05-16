9e0f76a5b51f834881fb186bd3d12653e19f26ef
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-05-16 23:28:09 +0200

    Added version numbers

--- DomoticzAPI/__init__.py
+++ DomoticzAPI/__init__.py
@@ -6,3 +6,21 @@ from .hardware import Hardware
 from .device import Device
 from .uservariable import UserVariable
 from .notification import Notification
+
+__version_major__ = 0
+__version_minor__ = 1
+__version_micro__ = 0
+
+__version__ = "{}.{}.{}".format(__version_major__, __version_minor__, __version_micro__)
+
+def version():
+    return __version__
+
+def version_major():
+    return __version_major__
+
+def version_minor():
+    return __version_minor__
+
+def version_micro():
+    return __version_micro__
