f7b10052a16d947f40ebd266b43ffe97329cbc26
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-05-20 22:56:19 +0200

    Extra tests for 'delete' and 'update'

--- test_hardware.py
+++ test_hardware.py
@@ -4,7 +4,7 @@ import DomoticzAPI as dom
 
 
 def main():
-    server = dom.Server(address="192.168.0.13")
+    server = dom.Server()
     # hw1 = dom.Hardware(server, "180")
     hw1 = dom.Hardware(server, idx="18")
     print(hw1)
@@ -14,10 +14,21 @@ def main():
     else:
         print("Hardware " + hw1.idx + " doesn't exists")
 
-    hw2 = dom.Hardware(server, Type=15, Port=1, Name="Sensors1", Enabled="true")
+    hw2 = dom.Hardware(server, Type=15, Port=1, Name="Sensors 1", Enabled="true")
     hw2.add()
     print(hw2)
-    print("{}: {} {}".format(hw2.name, hw2.api_status, hw2.api_title))
+    print("{}: {} - {}".format(hw2.name, hw2.api_status, hw2.api_title))
+    hw2.name = "Sensors 2"
+    hw2.update()
+    print("{}: {} - {}".format(hw2.name, hw2.api_status, hw2.api_title))
+    hw2.delete()
+    print(hw2)
+    print("{}: {} - {}".format(hw2.name, hw2.api_status, hw2.api_title))
+    hw2.address = "10.10.0.10"
+    hw2.serialport = "1234"
+    hw2.add()
+    print(hw2)
+    print("{}: {} - {}".format(hw2.name, hw2.api_status, hw2.api_title))
 
 
 if __name__ == "__main__":
