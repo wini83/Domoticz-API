1f6acf5800f3b64b09d20acb8e4e8b7492d354e6
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-05-24 21:14:11 +0200

    Update test_notification.py

--- test_notification.py
+++ test_notification.py
@@ -4,19 +4,20 @@ import DomoticzAPI as dom
 
 
 def main():
-    server = dom.Server(address="192.168.0.13")
+    server = dom.Server()
     notification = dom.Notification(server)
     notification.subject = "Test"
     notification.body = "Hello World!"
     print(notification)
     notification.send()
+
     print("Result = {}".format(notification.api_status))
     notification.subject = "Test only for Kodi"
     notification.subsystem = "kodi"
     print(notification)
     notification.send()
-    print("Result = {}".format(notification.api_status))
-    print("Title = {}".format(notification.api_title))
+
+    print("{}: {} - {}".format(notification, notification.api_status, notification.api_title))
 
 if __name__ == "__main__":
     main()
