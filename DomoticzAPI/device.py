762a0a9dffa8830b9e7055ff31b783391bed1db0
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-05-28 22:35:18 +0200

    Code reformatted and adding a device is working now

--- DomoticzAPI/device.py
+++ DomoticzAPI/device.py
@@ -3,90 +3,228 @@
 
 from .server import Server
 from .hardware import Hardware
+import datetime
+from urllib.parse import quote
 
 
 ################################################################################
 # Device                                                                       #
 ################################################################################
+
 class Device:
 
     _type_devices = "devices"
     _type_create_device = "createdevice"
+    _type_create_dummy = "createvirtualsensor"
 
-    _param_makefavorite = "makefavorite"
+    _type_delete_device = "deletedevice"
+
+    _param_make_favorite = "makefavorite"
+    _param_rename_device = "renamedevice"
+    _param_update_device = "udevice"
 
     _favorite_off = 0
     _favorite_on = 1
 
-    # Existing device: def __init__(self, server, idx)
-    # New device:      def __init__(self, server, hardwareidx, name, type=None, subtype=None):
     def __init__(self, server, *args, **kwargs):
         self._idx = None
-        self._HardwareID = None
+        self._Hardware = None
         self._Name = None
+        self._Type = None
+        self._SubType = None
+        self._TypeName = None
+        self._htype = None
         if isinstance(server, Server) and server.exists():
             self._server = server
         else:
             self._server = None
+        # Existing device: def __init__(self, server, idx)
         if len(args) == 1:
-            # For existing hardware
-            #   hw = dom.Hardware(server, "180")
+            # For existing device
+            #   hw = dom.Device(server, 180)
             self._idx = args[0]
-        elif len(args) >= 2:
+        # New device:      def __init__(self, server, hardware, name, type=None, subtype=None):
+        elif len(args) == 2:
             self._idx = None
             if isinstance(args[0], Hardware):
                 if args[0].exists():
-                    self._hardware = args[0]
-                else:
-                    self._hardware = None
-            self._Name = args[1]
-            if len(args) >= 3:
-                self._HardwareType = args[2]
-                if len(args) >= 4:
-                    self._SubType = args[3]
-                else:
-                    self._SubType = None
+                    self._Hardware = args[0]
             else:
-                self._HardwareType = None
+                self._Hardware = None
+            self._Name = args[1]
+            # Try to get named parameters
+            self._SubType = kwargs.get("subtype", None)
+            self._Type = kwargs.get("type", None)
+            self._TypeName = kwargs.get("typename", None)
         else:
             self._idx = kwargs.get("idx", None)
             if self._idx is None:
+                self._Hardware = kwargs.get("hardware", None)
                 self._Name = kwargs.get("name", None)
-                self._HardwareType = kwargs.get("type", None)
                 self._SubType = kwargs.get("subtype", None)
+                self._Type = kwargs.get("type", None)
+                self._TypeName = kwargs.get("typename", None)
+        if self._TypeName is not None:
+            self._Type = None
+            self._SubType = None
         self._initDevice()
 
     def __str__(self):
-        if self.exists():
-            return "{0}({1}, \"{2}\", \"{3}\")".format(self.__class__.__name__, str(self._server), self._idx, self._Name)
-        else:
-            return "None"
+        return "{}({}, {}, \"{}\")".format(self.__class__.__name__, str(self._server), self._idx, self._Name)
 
     # ..........................................................................
     # Public methods
     # ..........................................................................
+
     def add(self):
         if self._idx is None \
-                and self._HardwareID is not None \
+                and self._Hardware is not None \
                 and self._Name is not None \
-                and self._HardwareType is not None \
+                and self._Type is not None \
                 and self._SubType is not None:
-            # type=createdevice&idx=29&sensorname=Temp5&devicetype=50&devicesubtype=5
-            message = "type={}&idx={}&sensorname={}&devicetype={}&devicesubtype={}".format(self._type_create_device,
-                                                                                           self._HardwareID,
-                                                                                           self._Name,
-                                                                                           self._HardwareType,
-                                                                                           self._SubType)
-        pass
+            # type=createdevice&idx=IDX&sensorname=SENSORNAME&devicetype=DEVICETYPE&devicesubtype=SUBTYPE
+            querystring = "type={}".format(self._type_create_device)
+            querystring += "&idx={}".format(self._Hardware._idx)
+            querystring += "&sensorname={}".format(quote(self._Name))
+            querystring += "&devicetype={}".format(self._Type)
+            querystring += "&devicesubtype={}".format(self._SubType)
+            self._api_querystring = querystring
+            res = self._server._call_api(querystring)
+            self._api_status = res.get("status", self._server._return_error)
+            self._api_title = res.get("title", self._server._return_empty)
+            if self._api_status == self._server._return_ok:
+                self._idx = res.get("idx", None)
+                self._initDevice()
+
+    def delete(self):
+        if self.exists():
+            # type=deletedevice&idx=29
+            querystring = "type={}&idx={}".format(self._type_delete_device, self._idx)
+            self._api_querystring = querystring
+            res = self._server._call_command(querystring)
+            self._api_status = res.get("status", self._server._return_error)
+            self._api_title = res.get("title", self._server._return_empty)
+            if self._api_status == self._server._return_ok:
+                # self._Hardware = None
+                self._idx = None
 
     def exists(self):
-        return self._HardwareID is not None and self._idx is not None
+        return self._idx is not None and self._Hardware is not None
+
+    def hasBattery(self):
+        return not (self._BatteryLevel is None or self._BatteryLevel == 255)
 
     def isFavorite(self):
         return not (self._Favorite is None or self._Favorite == 0)
 
-    def hasBattery(self):
-        return not (self._BatteryLevel is None or self._BatteryLevel == 255)
+    def update(self, nvalue=0, svalue=""):
+        # /json.htm?type=command&param=udevice&idx=IDX&nvalue=NVALUE&svalue=SVALUE
+        if self.exists():
+            querystring = "param={}".format(self._param_update_device)
+            querystring += "&idx={}".format(self._idx)
+            querystring += "&nvalue={}".format(nvalue)
+            if len(svalue) > 0:
+                querystring += "&svalue={}".format(svalue)
+            res = self._server._call_command(querystring)
+            self._api_status = res.get("status", self._server._return_error)
+            self._api_title = res.get("title", self._server._return_empty)
+
+    # ..........................................................................
+    # Private methods
+    # ..........................................................................
+
+    def _initDevice(self):
+        if self._idx is not None:
+            querystring = "type=devices&rid={}".format(self._idx)
+        elif self._Name is not None:
+            querystring = "type=devices&filter=all&used=true&order=Name"
+        else:
+            querystring = ""
+        self._api_querystring = querystring
+        res = self._server._call_api(querystring)
+        self._api_status = res.get("status", self._server._return_error)
+        self._api_title = res.get("title", self._server._return_empty)
+        myDict = {}
+        if self._api_status == self._server._return_ok:
+            self._server._ActTime = res.get("ActTime")  # for some reason only given in device calls. No idea about the meaning!
+            # Update the server properties.
+            self._server._AstrTwilightEnd = res.get("AstrTwilightEnd")
+            self._server._AstrTwilightStart = res.get("AstrTwilightStart")
+            self._server._CivTwilightEnd = res.get("CivTwilightEnd")
+            self._server._CivTwilightStart = res.get("CivTwilightStart")
+            self._server._NautTwilightEnd = res.get("NautTwilightEnd")
+            self._server._NautTwilightStart = res.get("NautTwilightStart")
+            self._server._Sunrise = res.get("Sunrise")
+            self._server._Sunset = res.get("Sunset")
+            self._server._SunAtSouth = res.get("SunAtSouth")
+            self._server._DayLength = res.get("DayLength")
+            self._server._ServerTime = res.get("ServerTime")
+            result = res.get("result")
+            if result is not None:
+                if len(result) > 0:
+                    for resDict in result:
+                        if (self._idx is not None and resDict.get("idx") == self._idx) \
+                        or (self._Name is not None and resDict.get("Name") == self._Name):
+                            myDict = resDict
+                            break
+        self._AddjMulti = myDict.get("AddjMulti")
+        self._AddjMulti2 = myDict.get("AddjMulti2")
+        self._AddjValue = myDict.get("AddjValue")
+        self._AddjValue2 = myDict.get("AddjValue2")
+        self._BatteryLevel = myDict.get("BatteryLevel", 255)
+        self._CounterToday = myDict.get("CounterToday")
+        self._CustomImage = myDict.get("CustomImage")
+        self._Data = myDict.get("Data")
+        self._Description = myDict.get("Description")
+        self._DimmerType = myDict.get("DimmerType")
+        self._Favorite = myDict.get("Favorite")
+        self._HaveDimmer = myDict.get("HaveDimmer")
+        self._HaveGroupCmd = myDict.get("HaveGroupCmd")
+        self._HaveTimeout = myDict.get("HaveTimeout")
+        self._ID = myDict.get("ID")
+        self._idx = myDict.get("idx", self._idx)
+        self._Image = myDict.get("Image")
+        self._IsSubDevice = myDict.get("IsSubDevice")
+        self._LastUpdate = myDict.get("LastUpdate")
+        self._Level = myDict.get("Level")
+        self._LevelInt = myDict.get("LevelInt")
+        self._MaxDimLevel = myDict.get("MaxDimLevel")
+        self._Name = myDict.get("Name", self._Name)
+        self._Notifications = myDict.get("Notifications")
+        self._PlanID = myDict.get("PlanID")
+        self._PlanIDs = myDict.get("PlanIDs")
+        self._Protected = myDict.get("Protected")
+        self._SensorType = myDict.get("SensorType")
+        self._SensorUnit = myDict.get("SensorUnit")
+        self._ShowNotifications = myDict.get("ShowNotifications")
+        self._SignalLevel = myDict.get("SignalLevel")
+        self._Status = myDict.get("Status")
+        self._StrParam1 = myDict.get("StrParam1")
+        self._StrParam2 = myDict.get("StrParam2")
+        self._SubType = myDict.get("SubType", self._SubType)
+        self._SwitchType = myDict.get("SwitchType")
+        self._SwitchTypeVal = myDict.get("SwitchTypeVal")
+        self._Timers = myDict.get("Timers")
+        self._Type = myDict.get("Type", self._Type)
+        self._TypeImg = myDict.get("TypeImg")
+        self._Unit = myDict.get("Unit")
+        self._Used = myDict.get("Used")
+        self._UsedByCamera = myDict.get("UsedByCamera")
+        self._XOffset = myDict.get("XOffset")
+        self._YOffset = myDict.get("YOffset")
+
+        # Some info from the hardware also comes
+        HardwareID = myDict.get("HardwareID")
+        if HardwareID is not None:
+            hw = Hardware(self._server, idx=HardwareID)
+            if hw.exists():
+                self._Hardware = hw
+                # Following 3 values should be in de hardware class
+                # self._HardwareName = myDict.get("HardwareName")
+                self._Hardware._HardwareType = myDict.get("HardwareType")
+                # self._HardwareTypeVal = myDict.get("HardwareTypeVal")
+            else:
+                self._Hardware = None
 
     # ..........................................................................
     # Properties
@@ -99,6 +237,10 @@ class Device:
     @property
     def api_title(self):
         return self._api_title
+
+    @property
+    def api_querystring(self):
+        return self._api_querystring
 
     # ..........................................................................
 
@@ -151,27 +293,15 @@ class Device:
         # json.htm?type=command&param=makefavorite&idx=" + id + "&isfavorite=" + isfavorite
         if isinstance(value, bool) and self.exists():
             if value:
-                self._server._call_command(self._server._param.format(self._param_makefavorite) + "&idx={}&isfavorite={}".format(self._idx, str(self._favorite_on)))
+                self._server._call_command(self._server._param.format(self._param_make_favorite) + "&idx={}&isfavorite={}".format(self._idx, str(self._favorite_on)))
                 self._Favorite = self._favorite_on
             else:
-                self._server._call_command(self._server._param.format(self._param_makefavorite) + "&idx={}&isfavorite={}".format(self._idx,  str(self._favorite_off)))
+                self._server._call_command(self._server._param.format(self._param_make_favorite) + "&idx={}&isfavorite={}".format(self._idx, str(self._favorite_off)))
                 self._Favorite = self._favorite_off
 
     @property
-    def hardwareid(self):
-        return self._HardwareID
-
-    @property
-    def hardwarename(self):
-        return self._HardwareName
-
-    @property
-    def hardwaretype(self):
-        return self._HardwareType
-
-    @property
-    def hardwaretypeval(self):
-        return self._HardwareTypeVal
+    def hardware(self):
+        return self._Hardware
 
     @property
     def havedimmer(self):
@@ -188,6 +318,10 @@ class Device:
     @property
     def id(self):
         return self._ID
+
+    @property
+    def idx(self):
+        return self._idx
 
     @property
     def image(self):
@@ -216,6 +350,19 @@ class Device:
     @property
     def name(self):
         return self._Name
+
+    @name.setter
+    def name(self, value):
+        if self.exists():
+            # json.htm?type=command&param=renamedevice&idx=idx&name=
+            querystring = self._server._param.format(self._param_rename_device)
+            querystring += "&idx={}&name={}".format(self._idx, quoute(str(value)))
+            self._api_querystring = querystring
+            res = self._server._call_command(querystring)
+            self._api_status = res.get("status", self._server._return_error)
+            self._api_title = res.get("title", self._server._return_empty)
+            if self._api_status == self._server._return_ok:
+                self._Name = value
 
     @property
     def notifications(self):
@@ -277,6 +424,18 @@ class Device:
     def used(self):
         return self._Used
 
+    @used.setter
+    def used(self, value):
+        if value in ("true", "false") and self.exists():
+            # json.htm?type=setused&idx=<idx>&used=true
+            querystring = "type=setused&idx={}&used={}".format(self._idx, value)
+            self._api_querystring = querystring
+            res = self._server._call_api(querystring)
+            self._api_status = res.get("status", self._server._return_error)
+            self._api_title = res.get("title", self._server._return_empty)
+            if self._api_status == self._server._return_ok:
+                self._Used = value
+
     @property
     def xoffset(self):
         return self._XOffset
@@ -285,79 +444,3 @@ class Device:
     def yoffset(self):
         return self._YOffset
 
-    @property
-    def idx(self):
-        return self._idx
-
-    # ..........................................................................
-    # Private methods
-    # ..........................................................................
-    def _initDevice(self):
-        if self._idx is not None:
-            message = "type=devices&rid={}".format(self._idx)
-        elif self._Name is not None:
-            message = "type=devices&filter=all&used=true&order=Name"
-        else:
-            message = ""
-        res = self._server._call_api(message)
-        myDict = {}
-        if res.get("status") == self._server._return_ok:
-            result = res.get("result")
-            if result is not None:
-                if len(result) > 0:
-                    for resDict in result:
-                        if (self._idx is not None and resDict.get("idx") == self._idx) \
-                        or (self._Name is not None and resDict.get("Name") == self._Name):
-                            myDict = resDict
-                            break
-        self._AddjMulti = myDict.get("AddjMulti")
-        self._AddjMulti2 = myDict.get("AddjMulti2")
-        self._AddjValue = myDict.get("AddjValue")
-        self._AddjValue2 = myDict.get("AddjValue2")
-        self._BatteryLevel = myDict.get("BatteryLevel", 255)
-        self._CustomImage = myDict.get("CustomImage")
-        self._Data = myDict.get("Data")
-        self._Description = myDict.get("Description")
-        self._DimmerType = myDict.get("DimmerType")
-        self._Favorite = myDict.get("Favorite")
-        self._HardwareID = myDict.get("HardwareID")
-        self._HardwareName = myDict.get("HardwareName")
-        self._HardwareType = myDict.get("HardwareType")
-        self._HardwareTypeVal = myDict.get("HardwareTypeVal")
-        self._HaveDimmer = myDict.get("HaveDimmer")
-        self._HaveGroupCmd = myDict.get("HaveGroupCmd")
-        self._HaveTimeout = myDict.get("HaveTimeout")
-        self._ID = myDict.get("ID")
-        self._idx = myDict.get("idx", self._idx)
-        self._Image = myDict.get("Image")
-        self._IsSubDevice = myDict.get("IsSubDevice")
-        self._LastUpdate = myDict.get("LastUpdate")
-        self._Level = myDict.get("Level")
-        self._LevelInt = myDict.get("LevelInt")
-        self._MaxDimLevel = myDict.get("MaxDimLevel")
-        self._Name = myDict.get("Name", self._Name)
-        self._Notifications = myDict.get("Notifications")
-        self._PlanID = myDict.get("PlanID")
-        self._PlanIDs = myDict.get("PlanIDs")
-        self._Protected = myDict.get("Protected")
-        self._SensorType = myDict.get("SensorType")
-        self._SensorUnit = myDict.get("SensorUnit")
-        self._ShowNotifications = myDict.get("ShowNotifications")
-        self._SignalLevel = myDict.get("SignalLevel")
-        self._Status = myDict.get("Status")
-        self._StrParam1 = myDict.get("StrParam1")
-        self._StrParam2 = myDict.get("StrParam2")
-        self._SubType = myDict.get("SubType")
-        self._SwitchType = myDict.get("SwitchType")
-        self._SwitchTypeVal = myDict.get("SwitchTypeVal")
-        self._Timers = myDict.get("Timers")
-        self._Type = myDict.get("Type")
-        self._TypeImg = myDict.get("TypeImg")
-        self._Unit = myDict.get("Unit")
-        self._Used = myDict.get("Used")
-        self._UsedByCamera = myDict.get("UsedByCamera")
-        self._XOffset = myDict.get("XOffset")
-        self._YOffset = myDict.get("YOffset")
-        #
-        self._api_status = res.get("status")
-        self._api_title = res.get("title")
