3b2339da0f40a82d9da128d0d7ce86cf78cfb523
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-05-28 13:51:05 +0200

    Better tests

--- test_notification.py
+++ test_notification.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# --------------------------------------------------------------------------------
+# Look in Domoticz log for the results
+# --------------------------------------------------------------------------------
 import DomoticzAPI as dom
 
 
 def main():
     server = dom.Server()
-    notification = dom.Notification(server)
-    notification.subject = "Test"
-    notification.body = "Hello World!"
-    print(notification)
-    notification.send()
+    dom.Notification(server, subject="Test 1 subject", body="Hello World!").send()
 
-    print("Result = {}".format(notification.api_status))
-    notification.subject = "Test only for Kodi"
-    notification.subsystem = "kodi"
-    print(notification)
+    notification = dom.Notification(server, subject="Test 2 subject", body="Test body")
     notification.send()
 
     print("{}: {} - {}".format(notification, notification.api_status, notification.api_title))
+
+    notification.subject = "Test 3 only for Kodi"
+    notification.body = "Hello Kodi"
+    notification.subsystem = "kodi"
+    notification.send()
+
+    print("{}: {} - {}".format(notification, notification.api_status, notification.api_title))
+
 
 if __name__ == "__main__":
     main()
