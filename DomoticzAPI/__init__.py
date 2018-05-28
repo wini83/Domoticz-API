96e45f21bdc43fd43fb2e42e476edd89c41bc8ec
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-05-28 22:35:56 +0200

    Update __init__.py

--- DomoticzAPI/__init__.py
+++ DomoticzAPI/__init__.py
@@ -9,18 +9,22 @@ from .notification import Notification
 
 __version_major__ = 0
 __version_minor__ = 1
-__version_micro__ = 1
+__version_micro__ = 2
 
 __version__ = "{}.{}.{}".format(__version_major__, __version_minor__, __version_micro__)
+
 
 def version():
     return __version__
 
+
 def version_major():
     return __version_major__
 
+
 def version_minor():
     return __version_minor__
+
 
 def version_micro():
     return __version_micro__
