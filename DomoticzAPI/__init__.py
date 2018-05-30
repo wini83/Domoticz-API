8e9b9b5f1a2fd7693971c9ac9c5ca0c8c7b49580
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-05-30 18:44:30 +0200

    Version updated

--- DomoticzAPI/__init__.py
+++ DomoticzAPI/__init__.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .server import Server
-from .hardware import Hardware
+from .server import *
+from .hardware import *
 from .device import Device
-from .uservariable import UserVariable
-from .notification import Notification
+from .uservariable import *
+from .notification import *
 from .utilities import *
 
 __version_major__ = 0
-__version_minor__ = 1
-__version_micro__ = 2
+__version_minor__ = 2
+__version_micro__ = 0
 
 __version__ = "{}.{}.{}".format(__version_major__, __version_minor__, __version_micro__)
 
