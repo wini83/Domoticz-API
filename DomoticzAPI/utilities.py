159546eb5645ec8234edd9da10c86400a9114348
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-05-30 16:25:44 +0200

    Create utilities.py

---
+++ DomoticzAPI/utilities.py
@@ -0,0 +1,8 @@
+# Temperature conversion from Celsius to Fahrenheit
+def TempC2F(value):
+    return (value * 1.8) + 32
+
+
+# Temperature conversion from Fahrenheit to Celsius
+def TempF2C(value):
+    return (value - 32) / 1.8
