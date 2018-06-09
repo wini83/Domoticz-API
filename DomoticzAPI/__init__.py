699787d9794bfb80f5e129859cd720191c0247e5
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-06-09 23:43:33 +0200

    Removed 'version' to 'utilities.py'

--- DomoticzAPI/__init__.py
+++ DomoticzAPI/__init__.py
@@ -1,31 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from .server import *
 from .hardware import *
 from .device import *
 from .uservariable import *
 from .notification import *
 from .utilities import *
-
-__version_major__ = 0
-__version_minor__ = 2
-__version_micro__ = 0
-
-__version__ = "{}.{}.{}".format(__version_major__, __version_minor__, __version_micro__)
-
-
-def version():
-    return __version__
-
-
-def version_major():
-    return __version_major__
-
-
-def version_minor():
-    return __version_minor__
-
-
-def version_micro():
-    return __version_micro__
