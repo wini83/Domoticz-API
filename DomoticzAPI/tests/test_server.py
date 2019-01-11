3f55d02b36c354a0998e256b5c3172f104f87772
Xorfor <xorfor@hotmail.com>
2019-01-11 22:28:51 +0100

    Extra tests

--- DomoticzAPI/tests/test_server.py
+++ DomoticzAPI/tests/test_server.py
@@ -10,31 +10,41 @@ def main():
     print("********************************************************************************")
 
     server = dom.Server()
-    if server.api.status == server.api.OK:
-        print("server ................ : {}: {} - {}".format(server,
-                                                             server.api.status,
-                                                             server.api.title))
+    if server.exists():
+        print("server ................ : {}".format(server))
         print("Domoticz version ...... : {}".format(server.version))
         print("Build time ............ : {}".format(server.build_time_dt))
         print("DomoticzUpdateURL ..... : {}".format(server.domoticzupdateurl))
         print("Update available ...... : {}".format(server.haveupdate))
         print("\r")
 
+        print("--------------------------------------------------------------------------------")
+        print("SunRiseSet")
+        print("--------------------------------------------------------------------------------")
         print("Servertime ............ : {}".format(server.servertime))
         print("ServertimeDT .......... : {}".format(server.servertime_dt))
         print("Sunrise ............... : {}".format(server.sunrise))
         print("Sunset ................ : {}".format(server.sunset))
         print("SunsetDT .............. : {}".format(server.sunset_dt))
-        print("server ................ : {}: {} - {}".format(server,
-                                                             server.api.status,
-                                                             server.api.title))
         print("\r")
 
+        print("--------------------------------------------------------------------------------")
+        print("Log message")
+        print("--------------------------------------------------------------------------------")
         server.logmessage("Test 1")
         print("querystring ........... : {}".format(server.api.querystring))
-        print("server ................ : {}: {} - {}".format(server,
-                                                             server.api.status,
-                                                             server.api.title))
+        print("\r")
+
+        print("--------------------------------------------------------------------------------")
+        print("Settings")
+        print("--------------------------------------------------------------------------------")
+        print("AcceptNewHardware ..... : {}".format(
+            server.setting.value("AcceptNewHardware")))
+        print("Latitude .............. : {}".format(
+            server.setting.value("Location").get("Latitude")))
+        print("SecPassword ........... : {}".format(
+            server.setting.value("SecPassword")))
+
     else:
         print("Server not found!!!")
         print(server.api.message)
